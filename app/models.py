b5ef3ff903bfbc3ca9eccdaafc5fc2b8a1854359
Ivan_Kajfes <ivankajfes03@gmail.com>
2019-05-06 13:16:31 +0200

    models

---
+++ app/models.py
@@ -0,0 +1,58 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
+from flask_login import UserMixin
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    surname = db.Column(db.String(64), index=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    id_address = db.Column(db.Integer, db.ForeignKey('address.id'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+TripUser = Table('TripUser',
+    Column('id', Integer, primary_key=True),
+    Column('id_user', Integer, ForeignKey('user.id')),
+    Column('id_trip', Integer, ForeignKey('trip.id')))
+    
+
+class Address():
+    id = db.Column(db.Integer, primary_key=True)
+    street_name = db.Column(db.String(64), index=True)
+    street_number = db.Column(db.Integer, index=True)
+    postal_number = db.Column(db.Integer, index=True)
+    city = db.Column(db.String(64), index=True)
+
+
+class Trip():
+    id = db.Column(db.Integer, primary_key=True)
+    trip_name = db.Column(db.String(64), index=True)
+    max_number = db.Column(db.Integer, index=True)
+    start_date = db.Column(db.Integer, index=True)
+    end_date = db.Column(db.Integer, index=True)
+    price = db.Column(db.Integer, index=True)
+    destination = db.Column(db.String(64), index=True)
+    trip_description = db.Column(db.String(64), index=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+
+@login.user_loader
+
+def load_user(id):
+    return User.query.get(int(id))
