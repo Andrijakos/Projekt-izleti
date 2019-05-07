5d5dd2741c1a21db2beab89efc1b91e539183aae
Ivan_Kajfes <ivankajfes03@gmail.com>
2019-05-07 13:33:30 +0200

    bez migracija i idea

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+import datetime
+
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index/')
 def index():
+
     return render_template('index.html', title='Home')
